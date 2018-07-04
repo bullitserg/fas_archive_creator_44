e0168ba13e9f74193fad847f4743c3d07e4a0567
belims <bullit88@mail.ru>
2018-07-04 11:08:34 +0300

    Тестовая версия

---
+++ config.dist.py
@@ -0,0 +1,50 @@
+work_dir = 'C:/Users/belim/PycharmProjects/Архивы для ФАС 223/tmp'
+procedure_file = 'C:/Users/belim/PycharmProjects/Архивы для ФАС 223/tmp/procedures.list'
+
+requests_dir = 'Документы заявок'
+protocols_dir = 'Документы протоколов'
+explanations_dir = 'Документы разъяснений'
+features_dir = 'Документы преференций'
+organisation_data_dir = 'Аккредитационные документы организации'
+
+sleep_time = 3
+output_str_separator = ','
+
+dir_names_dict = {
+    'confirm-participant-compliance': 'Учредительные документы',
+    'firstPartDocuments': 'Документы, подтверждающие допуск по первым частям',
+    'other-documents': 'Прочие документы',
+    'confirm-requirements': 'Документы, подтверждающие соответствие по вторым частям',
+    'approve-large-transactions': 'Документы об одобрении больших сделок',
+    'conformity-declaration': 'Декларации о соответствии участника требованиям аукциона',
+    'founders-inn': 'Документы учредителей',
+    'offer': 'Документы саморегулируемой организации',
+    'other': 'Прочие документы',
+    'confirmParticipantCompliance': 'Документы подтверждения соответствие участников',
+    'foundersInn': 'Документы учредителей',
+    'requestdocument': 'Документы заявок',
+    'discharge': 'Документы отстранений',
+    'offerInfo': 'Документы саморегулируемой организации',
+    'requestReview': 'Протокол рассмотрения первых частей заявок',
+    'requestReviewFinal': 'Протокол рассмотрения вторых частей заявок',
+    'organizationDocument_9': 'Копия выписки из ЕГРЮЛ',
+    'organizationDocument_10': 'Копии учредительных документов (Устав)',
+    'organizationDocument_11': 'Документы о государственной регистрации',
+    'organizationDocument_12': 'Копии документов, подтверждающих полномочия лица на получение аккредитации',
+    'organizationDocument_13': 'Копии документов, подтверждающих полномочия руководителя',
+    'organizationDocument_14': 'Копия решения об одобрении или о совершении по результатам электронных сделок',
+    'organizationDocument_15': 'Копия документа, удостоверяющего личность',
+    'organizationDocument_16': 'Копия выписки из единого государственного реестра ИП',
+    'organizationDocument_20': 'Другие документы'
+}
+
+document_dirs = {'s223:///': '/upl/srv/www/sectionks-archive/data/upload/',
+                 'fz94:///': '/upl/srv/www/auction-archive/public/upload/',
+                 'fz94_1:///': '/upl/srv/www/auction-archive/public/upload1/',
+                 'fz94_2:///': '/upl/srv/www/auction-archive-upload2/public/upload2/',
+                 'fz94_5:///': '/upl/srv/www/auction-archive-upload5/',
+                 'fz94_7:///': '/upl/srv/www/auction/public/upload7/',
+                 'local:///': '/upl/srv/www/sectionks-archive/data/upload/',
+                 'local3:///': '/upl/srv/www/sectionks-archive/data/upload3/',
+                 'local6:///': '/upl/srv/www/sectionks/data/upload6/'
+                 }
---
+++ queries.py
@@ -0,0 +1,97 @@
+get_requests_data_query = '''SELECT
+  rd.type AS docType,
+  r.ordinalNumber AS rNum,
+  IFNULL(rd.documentType, rd.type) AS rDocType,
+  CONCAT(rd.id, '_', rd.realName) AS docName,
+  rd.uri
+FROM procedureRequest r
+  JOIN procedures p
+    ON p.id = r.procedureId
+  JOIN procedureRequestDocument rd
+    ON rd.requestId = r.id
+WHERE p.registrationNumber = '%(procedureNumber)s'
+AND r.actualId IS NULL
+AND r.requestStatusId != 19
+;'''
+
+
+get_protocols_data_query = '''SELECT
+  'protocol' AS docType,
+  pd.type AS pDocType,
+  CONCAT(pd.id, '_', pd.realName) AS docName,
+  pd.uri
+FROM procedures p
+  JOIN procedureProtocol p1
+    ON p1.procedureId = p.id
+    AND p1.actualId IS NULL
+    AND p1.status = 24
+  JOIN procedureProtocolDocument pd
+    ON pd.protocolId = p1.id
+WHERE p.registrationNumber = '%(procedureNumber)s'
+AND p.actualId IS NULL;'''
+
+
+get_features_data_query = '''SELECT
+  'explanation' AS docType,
+  CONCAT(ed.id, '_', ed.realName) AS docName,
+  ed.uri
+FROM procedures p
+  JOIN procedureExplanation e
+    ON e.procedureId = p.id
+  JOIN procedureExplanationDocument ed
+    ON ed.explanationId = e.id
+WHERE p.registrationNumber = '%(procedureNumber)s'
+AND e.discriminator = 'request'
+AND p.actualId IS NULL;'''
+
+
+get_explanations_data_query = '''SELECT
+  'features' AS docType,
+  r.ordinalNumber AS rNum,
+  CONCAT(rfd.id, '_', rfd.realName) AS docName,
+  rfd.uri
+FROM procedures p
+  JOIN procedureRequest r
+    ON r.procedureId = p.id AND r.actualId IS NULL AND r.requestStatusId != 19
+  JOIN procedureRequestFeature rf
+    ON r.id = rf.requestId
+  JOIN procedureRequestFeatureDocument rfd
+    ON rfd.requestFeatureId = rf.id
+WHERE p.registrationNumber = '%(procedureNumber)s'
+AND p.actualId IS NULL;'''
+
+
+get_organisation_data_query = '''SELECT
+  'organizationDocument' AS docType,
+  CONCAT('organizationDocument_', d.typeId) AS  pDocType,
+  r.ordinalNumber AS requestOrdinalNumber,
+  CONCAT(d.id, '_', d.realName) AS docName,
+  d.uri
+FROM procedures p
+  JOIN procedureRequest r
+    ON r.procedureId = p.id AND r.requestStatusId != 19
+  JOIN organization o ON o.id = r.organizationId AND o.actualId IS NULL
+  JOIN organizationDocument d ON d.organizationId = o.id
+WHERE p.registrationNumber = '%(procedureNumber)s'
+AND p.actualId IS NULL;
+'''
+
+
+get_offers_data_query = '''SELECT
+  r.id AS 'Номер заявки участника',
+  org.fullName AS 'Наименование участника',
+  org.inn AS 'ИНН',
+  IFNULL(ABS(o.offer),'') AS 'Предложенная цена, руб.',
+  IF(o.offer IS NOT NULL,IF(o.offer>0,'На понижение v','На повышение ^'),'') AS 'Тип ценового предложения',
+  IFNULL(o.createDateTime,'') AS 'Дата и время подачи предложения',
+  IF(o.valid=1,'+','') AS 'C учетом шага цены'
+FROM procedures p
+  JOIN procedureRequest r
+    ON r.procedureId = p.id AND r.actualId IS NULL AND r.requestStatusId != 19
+  JOIN organization org
+    ON org.id = r.organizationId AND org.actualId IS NULL
+  JOIN procedureOffer o
+    ON o.procedureId = p.id AND o.requestId = r.id
+WHERE p.registrationNumber = '%(procedureNumber)s'
+ORDER BY p.registrationNumber, o.number DESC
+;'''
